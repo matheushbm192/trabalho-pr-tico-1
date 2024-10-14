05ccb1fd98dbda1ca5ccb3d9fa5579db717e7731
Matheus H B Miranda <matheushbm192@gmail.com>
2024-10-14 09:44:00 -0300

    First commit

---
+++ Main/src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void sair(){
+
+    }
+    public static void iniciarJogo(){
+
+    }
+    public static void instrucoes(){
+
+    }
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("      Bem vindo ao Racha Cuca \n - Sair - (0) \n - Deseja iniciar um novo jogo - (1) \n - Instruções de jogo - (2) \n ");
+
+        int escolha = entrada.nextInt();
+
+        switch (escolha) {
+            case 0:
+                sair();
+            break;
+            case 1:
+                iniciarJogo();
+            break;
+            case 2:
+                instrucoes();
+            break;
+            default:
+                System.out.println("O valor inserido é invalido! \nTente novamente!");
+                String[] ola = {"ola", "fusca"};
+                main(ola);
+        }
+    }
+}
